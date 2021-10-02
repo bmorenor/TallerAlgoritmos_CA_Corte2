a3b415343bce5ee5b736a42c30515dfb6c72eff6
Andres <Andres@LAPTOP-VQ550K0A>
2021-10-02 00:22:51 -0500

    holawas

--- TallerAlgoritmos_CA_Corte2/src/co/edu/unbosque/controller/AplMain.java
+++ TallerAlgoritmos_CA_Corte2/src/co/edu/unbosque/controller/AplMain.java
@@ -1,12 +1,14 @@
 package co.edu.unbosque.controller;
+
 import co.edu.unbosque.view.VentanaPrincipal;
 
 public class AplMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Controller controller = new Controller();
+		Controller controller = new Controller();
 
+		System.out.println("holawas");
 	}
 
 }
